6d8ae3e056d77789078fea3b54e4f8c0b8aaa36d
Nazareno Viero <nazarenoviero@gmail.com>
2023-01-04 06:19:50 -0300

    Update Ingresar btn

--- Portfolio_Viero/src/app/app.module.ts
+++ Portfolio_Viero/src/app/app.module.ts
@@ -5,7 +5,6 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
 import { LogoAPComponent } from './components/logo-ap/logo-ap.component';
-import { SocialComponent } from './components/social/social.component';
 import { BannerComponent } from './components/banner/banner.component';
 import { AboutComponent } from './components/about/about.component';
 import { ExperienceComponent } from './components/experience/experience.component';
@@ -19,7 +18,6 @@ import { FooterComponent } from './components/footer/footer.component';
     AppComponent,
     HeaderComponent,
     LogoAPComponent,
-    SocialComponent,
     BannerComponent,
     AboutComponent,
     ExperienceComponent,
